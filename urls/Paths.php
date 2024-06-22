f72daf95bba8f86814b005091ea1d62fe5fbaea7
guiribajustin2004 <127721330+guiribajustin2004@users.noreply.github.com>
2024-06-22 21:50:47 +0800

    BUG FIXED

--- urls/Paths.php
+++ urls/Paths.php
@@ -13,7 +13,6 @@ class path {
 
             if (isset($new_uri[1]) && $new_uri[1] === $path['path'] || $uri === $path['path']) {
                 require 'views/' . $path['name'] . '.php';
-                require 'views/' . $path['name'] . '.php';
                 return;
             }   
 
